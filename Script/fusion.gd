10e8e680c5a64cd1795c1d0091a503be1132166b
santiago22 <tasiomartin33@gmail.com>
2026-02-24 12:48:53 +0100

    mecanica de fusion finalizada

--- Script/fusion.gd
+++ Script/fusion.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 200.0 
-const JUMP_VELOCITY = -350.0
-const WILSON_SCENE = preload("res://Scenes/wilson.tscn")
-const LULU_SCENE = preload("res://Scenes/Lulu.tscn")
+const SPEED = 250.0 
+const JUMP_VELOCITY = -500.0
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -16,23 +14,33 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = direction < 0
-		$AnimatedSprite2D2.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("Interact"):
 		separate()
 
 func separate():
-	var wilson = WILSON_SCENE.instantiate()
-	var lulu = LULU_SCENE.instantiate()
 	
-	lulu.global_position = global_position
-	wilson.global_position = global_position + Vector2(-20, -10) 
-
-	get_parent().add_child(lulu)
-	get_parent().add_child(wilson)
-
+	var wilson_inst=load("res://Scenes/wilson.tscn").instantiate()
+	var lulu_inst=load("res://Scenes/lulu.tscn").instantiate()
+	
+	lulu_inst.global_position = global_position
+	wilson_inst.global_position = global_position + Vector2(-35, -20) 
+	
+	lulu_inst.is_active = true
+	wilson_inst.is_active = false
+	
+	lulu_inst.modulate.a = 1.0
+	wilson_inst.modulate.a= 0.5
+	
+	var mundo=get_parent()
+	mundo.add_child(lulu_inst)
+	mundo.add_child(wilson_inst)
+	
+	if lulu_inst.has_node("Camera2D"):
+		lulu_inst.get_node("Camera2D").make_current()
+	
 	queue_free()
--- Script/lulu.gd
+++ Script/lulu.gd
@@ -2,39 +2,45 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
-const FUSION_SCENE = preload("res://Scenes/fusion.tscn") 
-
+var is_active = true
 var wilson_nearby = false 
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	if is_active:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("Move Left", "Move Right")
-	if direction:
-		velocity.x = direction * SPEED
+		var direction = Input.get_axis("Move Left", "Move Right")
+		if direction:
+			velocity.x = direction * SPEED
+			$AnimatedSprite2D.flip_h = direction < 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x,0,SPEED)
+			
 	move_and_slide()
 
-func interact():
-	if wilson_nearby:
-		transform_now()
+	if Input.is_action_just_pressed("Interact"):
+		if wilson_nearby:
+			transform_now()
 
 func transform_now():
-	var fusion = FUSION_SCENE.instantiate()
+	if get_parent().has_node("Fusion"): 
+		return
+
+	var fusion = load("res://Scenes/fusion.tscn").instantiate()
 	fusion.global_position = global_position 
-	get_parent().add_child(fusion)
 	
-	var wilson = get_parent().get_node("Wilson")
-	if wilson: 
+	get_parent().add_child(fusion)
+	var wilson = get_parent().find_child("Wilson", true, false)
+	if wilson:
 		wilson.queue_free()
 	
-	queue_free() 
+	queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Wilson":
