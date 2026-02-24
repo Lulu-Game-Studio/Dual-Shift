a621743ead2cb1a12ce503e26b6dc63df65228b9
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-02-24 09:15:32 +0100

    Character Fusion In Process

--- Script/fusion.gd
+++ Script/fusion.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
+const SPEED = 200.0 
+const JUMP_VELOCITY = -350.0
+const WILSON_SCENE = preload("res://Scenes/wilson.tscn")
+const LULU_SCENE = preload("res://Scenes/Lulu.tscn")
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -600.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Move Left", "Move Right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0
+		$AnimatedSprite2D2.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("interact"):
+		separate()
+
+func separate():
+	var wilson = WILSON_SCENE.instantiate()
+	var lulu = LULU_SCENE.instantiate()
+	
+	lulu.global_position = global_position
+	wilson.global_position = global_position + Vector2(-20, -10) 
+
+	get_parent().add_child(lulu)
+	get_parent().add_child(wilson)
+
+	queue_free()
--- Script/lulu.gd
+++ Script/lulu.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+const FUSION_SCENE = preload("res://Scenes/fusion.tscn") 
+
+var wilson_nearby = false 
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -17,3 +20,26 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func interact():
+	if wilson_nearby:
+		transform_now()
+
+func transform_now():
+	var fusion = FUSION_SCENE.instantiate()
+	fusion.global_position = global_position 
+	get_parent().add_child(fusion)
+	
+	var wilson = get_parent().get_node("Wilson")
+	if wilson: 
+		wilson.queue_free()
+	
+	queue_free() 
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Wilson":
+		wilson_nearby = true
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Wilson":
+		wilson_nearby = false
