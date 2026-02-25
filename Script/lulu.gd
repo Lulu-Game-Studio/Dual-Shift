5ad9589ccb5bbcb81d71bac993f5cbf06c732c07
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-02-25 09:22:19 +0100

    Box movement for Lulu Created and Fixed Lulu's Camera

--- Script/lulu.gd
+++ Script/lulu.gd
@@ -4,6 +4,8 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 var is_active = true
 var wilson_nearby = false 
+# He subido la fuerza a 500 para que se note el movimiento
+@export var push_force = 500.0 
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -20,10 +22,24 @@ func _physics_process(delta):
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x,0,SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 			
 	move_and_slide()
-
+	
+	# LÓGICA DE EMPUJE MEJORADA
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var body = collision.get_collider()
+		
+		if body is RigidBody2D:
+			# PASO CLAVE: Si la caja está congelada, la descongelamos
+			if body.freeze:
+				body.freeze = false
+			
+			# Aplicamos el impulso. 
+			# Usamos el normal de la colisión invertido para empujar hacia adelante
+			body.apply_central_impulse(collision.get_normal() * -push_force)
+	
 	if Input.is_action_just_pressed("Interact"):
 		if wilson_nearby:
 			transform_now()
