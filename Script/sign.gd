7d8508e1b05f31d18e4144320e64dda6f71c2c8c
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-02-25 13:04:39 +0100

    Updated Sign.gd

--- Script/sign.gd
+++ Script/sign.gd
@@ -4,9 +4,9 @@ func _ready():
 	$Panel.hide() 
 
 func _on_body_entered(body):
-	if body is CharacterBody2D:
+	if body.name == "Lulu" or body.name == "Wilson":
 		$Panel.show()
 
 func _on_body_exited(body):
-	if body is CharacterBody2D:
+	if body.name == "Lulu" or body.name == "Wilson":
 		$Panel.hide()
