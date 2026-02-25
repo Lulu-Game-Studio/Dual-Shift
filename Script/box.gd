752a92f6f5708e720ac68093ae815917eaf0c5fa
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-02-25 12:49:08 +0100

    Code improvement and debug code removed.
    Code improvement in sign.gd and debug code deleted in box.gd

--- Script/box.gd
+++ Script/box.gd
@@ -4,7 +4,6 @@ func _ready():
 	lock_rotation = true
 	freeze = true
 	contact_monitor = true
-	max_contacts_reported = 5
 	
 	can_sleep = false 
 
@@ -15,6 +14,5 @@ func _physics_process(_delta):
 		if body.is_in_group("Lulu"):
 			if freeze:
 				freeze = false
-				print("¡Lulu detectada! Caja desbloqueada.")
 		else:
 			pass
--- Script/sign.gd
+++ Script/sign.gd
@@ -1,16 +1,12 @@
 extends Area2D
 
-@export_multiline var sign_text : String = "Write your message here"
-@onready var message_label = $Label
-
 func _ready():
-	message_label.text = sign_text
-	message_label.hide()
+	$Panel.hide() 
 
 func _on_body_entered(body):
-	if body.is_in_group("Lulu") or body.is_in_group("Wilson") or body.is_in_group("fusion"):
-		message_label.show()
+	if body is CharacterBody2D:
+		$Panel.show()
 
 func _on_body_exited(body):
-	if body.is_in_group("Lulu") or body.is_in_group("gato") or body.is_in_group("fusion"):
-		message_label.hide()
+	if body is CharacterBody2D:
+		$Panel.hide()
