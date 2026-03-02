0b53be94b5c3a452b9d5eec7a5bc06c94080e8b4
santiago22 <tasiomartin33@gmail.com>
2026-03-02 09:39:34 +0100

    level 2 improved

--- Script/blocker.gd
+++ Script/blocker.gd
@@ -24,3 +24,12 @@ func _on_button_3_actived() -> void:
 
 func _on_button_3_disabled() -> void:
 	anim.play_backwards("open")
+
+
+
+func _on_button_4_actived() -> void:
+	anim.play("open")
+
+
+func _on_button_4_disabled() -> void:
+	anim.play_backwards("open")
