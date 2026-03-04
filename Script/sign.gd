56b68fc053d19d51133aeea7316a067c428c3ff9
santiago22 <tasiomartin33@gmail.com>
2026-03-04 11:12:08 +0100

    Ultimos detalles del juego

--- Script/sign.gd
+++ Script/sign.gd
@@ -4,9 +4,9 @@ func _ready():
 	$Panel.hide() 
 
 func _on_body_entered(body):
-	if body.name == "Lulu" or body.name == "Wilson":
+	if body.name == "Lulu" or body.name == "Wilson" or body.name == "Fusion":
 		$Panel.show()
 
 func _on_body_exited(body):
-	if body.name == "Lulu" or body.name == "Wilson":
+	if body.name == "Lulu" or body.name == "Wilson" or body.name == "Fusion":
 		$Panel.hide()
